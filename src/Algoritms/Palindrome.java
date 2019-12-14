a6a9ec0af1956c589045ae913adfec8318dfe760
Andrii V <you@example.com>
2019-12-14 07:53:56 -0800

    Added Palindrome method

--- src/Algoritms/Palindrome.java
+++ src/Algoritms/Palindrome.java
@@ -1,4 +1,38 @@
 package Algoritms;
 
 public class Palindrome {
+
+
+//    For worlds
+
+    public static boolean isPalindrome (String word) {
+
+        boolean result = false;
+        char[] array = word.toCharArray();
+        for(int i = 0; i < array.length/2; i++) {
+            if (array [i] != array[array.length-1-i]) {
+                result = false;
+                return result;
+            }
+        }
+        result = true;
+        return result;
+    }
+
+
+//    For numbers
+
+    public static boolean isPalindrome1(int b) {
+        boolean result = false;
+        String a = Integer.toString(b);
+        char[] array = a.toCharArray();
+        for (int i = 0; i < array.length/2; i++) {
+            if (array [i] != array[array.length-1-i]) {
+                result = false;
+                return result;
+            }
+        }
+        result = true;
+        return result;
+    }
 }
